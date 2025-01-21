c4e554103de14a6a0a4536d08101e074a36b23a8
Vladislav Otdelyanov <vlad_1995@mail.ru>
2025-01-21 14:39:04 +0300

    Изменены файлы проекта.

---
+++ CurrencyConverter/Model/Currency.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CurrencyConverter.Model
+{
+    internal class Currency
+    {
+        public DateTime Date { get; set; }
+        public DateTime PreviousDate { get; set; }
+        public string PreviousURL { get; set; }
+        public DateTime Timestamp { get; set; }
+        public Dictionary<string, Valute> Valute { get; set; }
+    }
+}
---
+++ CurrencyConverter/View/Windows/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using CurrencyConverter.Model;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CurrencyConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CurrrencyService _currencyService;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _currencyService = new CurrrencyService();
+        }
+
+        private void SellCmb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        private void PurchaseCmb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private async void UpdateCourseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            await _currencyService.LoadCurrencyAsync(CurrencyLv);
+            _currencyService.LoadValutes(SellCmb);
+            _currencyService.LoadValutes(PurchaseCmb);
+        }
+
+        private void ConvertBtn_Click(object sender, RoutedEventArgs e)
+        {
+            double sellAmount = Convert.ToDouble(SellTb.Text);
+            Valute sellValute = SellCmb.SelectedItem as Valute;
+            Valute buyValute = PurchaseCmb.SelectedItem as Valute;
+
+            BuyTb.Text = _currencyService.ConvertValute(sellAmount, sellValute, buyValute);
+        }
+    }
+}
