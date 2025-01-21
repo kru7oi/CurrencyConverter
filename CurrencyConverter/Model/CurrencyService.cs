b210bcdf520c4996eaa6bab415b7d6a9b18892c7
Vladislav Otdelyanov <vlad_1995@mail.ru>
2025-01-21 16:59:01 +0300

    Добавлены файлы проекта.

---
+++ CurrencyConverter/Model/CurrencyService.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CurrencyConverter.Model
+{
+    public class CurrencyService
+    {
+        private const string JSON_PATH = "https://www.cbr-xml-daily.ru/daily_json.js";
+        public List<Valute> Valutes { get; private set; }
+        public Currency Currency { get; private set; }
+
+        public async Task LoadCurrencyAsync(ListView listView)
+        {
+            HttpClient httpClient = new();
+            var response = await httpClient.GetStringAsync(JSON_PATH);
+
+            if (!string.IsNullOrEmpty(response))
+            {
+                Currency = JsonConvert.DeserializeObject<Currency>(response);
+
+                Valutes = Currency.Valute.Values.ToList();
+
+                Valutes.Insert(0, new()
+                {
+                    ID = "R00001",
+                    NumCode = "643",
+                    CharCode = "RUB",
+                    Nominal = 1,
+                    Name = "Российский рубль",
+                    Value = 1,
+                    Previous = 1
+                });
+
+                listView.ItemsSource = Currency.Valute.Values;
+            }
+        }
+
+        public void LoadValutes(ComboBox comboBox)
+        {
+            comboBox.ItemsSource = Valutes;
+        }
+    }
+}
---
+++ CurrencyConverter/View/Windows/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using CurrencyConverter.Model;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CurrencyConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CurrencyService _currencyService;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _currencyService = new CurrencyService();
+        }
+
+
+        private void SellCmb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        private void PurchaseCmb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        private void UpdateCourseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _currencyService.LoadCurrencyAsync(CurrencyLv);
+            _currencyService.LoadValutes(SellCmb);
+            _currencyService.LoadValutes(PurchaseCmb);
+        }
+        private void ConvertBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
