5907fef1b1b5a145421bedcf08e973aa9a857a17
Vladislav Otdelyanov <vlad_1995@mail.ru>
2025-01-21 11:01:13 +0300

    Реализован класс валют.

--- CurrencyConverter/Model/Valute.cs
+++ CurrencyConverter/Model/Valute.cs
@@ -2,5 +2,10 @@
 {
     public class Valute
     {
+        public string ID { get; set; }
+        public string NumCode { get; set; }
+        public string CharCode { get; set; }
+        public int Nominal { get; set; }
+        public string Name { get; set; }
     }
 }
